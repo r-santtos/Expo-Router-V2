a772ff32caa280efaf3e206d7358e91b2eecf7e8
r-santtos <rsanttos.dev@gmail.com>
2023-09-07 13:48:06 -0300

    config google

--- app/context/auth.tsx
+++ app/context/auth.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useStorageState } from '../server_config/useStorageState';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser'
 
-const AuthContext = React.createContext<{ signIn: () => void; signOut: () => void; session?: string | null, isLoading: boolean } | null>(null);
+WebBrowser.maybeCompleteAuthSession();
 
-// This hook can be used to access the user info.
+const AuthContext = createContext<{
+  signIn: () => void;
+  signOut: () => void;
+  session?: string | null;
+  isLoading: boolean;
+} | null>(null);
+
 export function useSession() {
-  const value = React.useContext(AuthContext);
+  const value = useContext(AuthContext);
   if (process.env.NODE_ENV !== 'production') {
     if (!value) {
       throw new Error('useSession must be wrapped in a <SessionProvider />');
@@ -18,19 +26,41 @@ export function useSession() {
 export function SessionProvider(props) {
   const [[isLoading, session], setSession] = useStorageState('session');
 
+  // Adicione um estado para a resposta da autenticação do Google
+  const [googleAuthResponse, setGoogleAuthResponse] = useState(null);
+
+  // Adicione um efeito para verificar a resposta da autenticação do Google
+  useEffect(() => {
+    if (googleAuthResponse && googleAuthResponse.type === 'success') {
+      setSession('xxx');
+    }
+  }, [googleAuthResponse]);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '597178002208-ujtq094m9dsmrba01vj1od440jbau6s8.apps.googleusercontent.com',
+    androidClientId: '872260821904-7ct5pjep6scsaaj259q3vqf8a4prqai1.apps.googleusercontent.com',
+    iosClientId: '597178002208-1sqv4tlu5go8mj39cpunn3gkok1rooif.apps.googleusercontent.com'
+  });
+
+  useEffect(() => {
+    if (response) {
+      setGoogleAuthResponse(response);
+    }
+  }, [response]);
+
   return (
     <AuthContext.Provider
       value={{
         signIn: () => {
-          // Perform sign-in logic here
-          setSession('xxx');
+          promptAsync();
         },
         signOut: () => {
           setSession(null);
         },
         session,
         isLoading,
-      }}>
+      }}
+    >
       {props.children}
     </AuthContext.Provider>
   );
